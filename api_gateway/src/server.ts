c94fb6f836c744c86f98f8bfff32081898089802
unknown <bruno.emedeiros99@gmail.com>
2023-04-26 12:10:09 -0300

    cors

--- api_gateway/src/server.ts
+++ api_gateway/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(router);
+app.use(cors);
 
 app.listen(8000, async ()=>{
     console.log("api_gateway running on 8000 port");
