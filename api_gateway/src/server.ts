037e3e0afc7beee9d42d1f8a05b74c6564eeca74
unknown <bruno.emedeiros99@gmail.com>
2023-04-27 19:36:30 -0300

    cors 2

--- api_gateway/src/server.ts
+++ api_gateway/src/server.ts
@@ -6,14 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(router);
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+
 
 app.listen(8000, async ()=>{
     console.log("api_gateway running on 8000 port");
